714147abbd4c919f22304cf309841d68be454f7f
Carlos Moroni Garcia <carlosmoronisud@gmail.com>
2025-05-07 12:08:34 -0300

    :sparkles: Inserido a classe Menu, Util, Exeptions

---
+++ src/projeto_final_bloco_01/Menu.java
@@ -0,0 +1,80 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+
+import conta_bancaria.util.BancoExceptions;
+import conta_bancaria.util.Cores;
+
+public class Menu {
+    public static void main(String[] args) {
+       
+        Scanner leia = new Scanner(System.in);
+        
+        int opcao;
+       do {
+            System.out.println("\n=== BOOKSTORE ===");
+            System.out.println("1. Cadastrar Livro Físico");
+            System.out.println("2. Cadastrar E-book");
+            System.out.println("3. Listar Todos os Produtos");
+            System.out.println("4. Buscar Produto por ID");
+            System.out.println("5. Listar apenas Livros Físicos");
+            System.out.println("6. Listar apenas E-books");
+            System.out.println("0. Sair");
+            System.out.print("Opção: ");
+            
+            opcao = leia.nextInt();
+            leia.nextLine();
+            
+            if (opcao == 0) {
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nAdeus!!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+            
+            switch(opcao) {
+	            case 1:
+	                System.out.println("Cadastrar livro físico");
+	                BancoExceptions.keyPress();
+	                break;
+	            case 2:
+	            	System.out.println("Cadastrar e-book");
+	            	BancoExceptions.keyPress();
+	                break;
+	            case 3:
+	                System.out.println("Listar");
+	                BancoExceptions.keyPress();
+	                break;
+	            case 4:
+	            	System.out.println("Buscar produto por id");
+	            	BancoExceptions.keyPress();
+	                break;
+	            case 5:
+	                System.out.println("Listar apenas livros físicos");
+	                BancoExceptions.keyPress();
+	                break;
+	            case 6:
+	            	System.out.println("Listar apenas E-books");
+	            	BancoExceptions.keyPress();
+	                break;
+	                
+	            default:
+					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+					
+                    break;
+            }
+            
+    	}while (opcao != 0);
+           
+        
+    
+        
+    }
+        public static void sobre() {
+        	System.out.println("\n*********************************************************");
+        	System.out.println("Projeto Desenvolvido por: ");
+        	System.out.println("Carlos Moroni Garcia - carlosmoronisud@gmail.com");
+        	System.out.println("https://github.com/carlosmoronisud");
+        	System.out.println("*********************************************************");
+           }
+}
